d075033fca74bf487b2c1c997d77ec231240b94a
bsharp1001 <bsharp0110@gmail.com>
2021-11-01 04:00:51 +0200

    env

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -10,14 +10,14 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "version.json": "50da16b76c6f2884dfcbb713b39f9265",
 "manifest.json": "c714ef8b85932f0c006da9284c0bccfe",
-"index.html": "c5f412cf05ff840e508ef7e125a29aa0",
-"/": "c5f412cf05ff840e508ef7e125a29aa0",
+"index.html": "0fa14363b55bd266f34284afb1a4d179",
+"/": "0fa14363b55bd266f34284afb1a4d179",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "d417bc05d5f3c1bfc29aca34fd99871e"
+"main.dart.js": "8d5ca47e261cebf645d3fea8371434b1"
 };
 
 // The application shell files that are downloaded before a service worker can
