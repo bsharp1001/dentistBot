8cecceda7f1af682fe58462febee58c1ba112f58
bsharp1001 <bsharp0110@gmail.com>
2021-11-01 21:51:43 +0200

    ttr

--- index.js
+++ index.js
@@ -45,11 +45,14 @@ router.post("/secreat_chat_patht", async request => {
   if (request.headers.get("Content-Type") === "application/json") {
     fields = await request.json()
   }
-  //DENTIST_TELEGRAM_BOT.put('update', JSON.stringify(fields));
+  DENTIST_TELEGRAM_BOT.put('update', typeof fields.message);
+  DENTIST_TELEGRAM_BOT.put('updat2e', typeof fields.message.chat);
+  DENTIST_TELEGRAM_BOT.put('updat22e', fields.message.chat);
+
   //return new Response(fields)
   var chat_ids = DENTIST_TELEGRAM_BOT.get('chat_ids', {'type': 'json'}) || [];
-  if (chat_ids.findIndex(fields.chat.id) == -1) {
-    chat_ids.push(fields.chat.id);
+  if (chat_ids.findIndex(fields.message.chat.id) == -1) {
+    chat_ids.push(fields.message.chat.id);
   }
   DENTIST_TELEGRAM_BOT.put('chat_ids', JSON.stringify(chat_ids));
 })
